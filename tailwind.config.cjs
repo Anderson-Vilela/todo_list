9e17a82e45c781bd66f190f285a644f27ce45f05
Anderson.Vilela <andersonvilela.dev@gmail.com>
2023-01-02 15:06:37 -0300

    ajustes nos valores padrões do projeto e variáveis padrões

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,6 +24,27 @@ module.exports = {
         'extraSmall-only': { min: '475px', max: '639px' },
         'extraSmall-down': { max: '474px' },
       },
+      colors: {
+        purple: {
+          DEFAULT: '#8284FA',
+          dark: '#5E60CE',
+        },
+        blue: {
+          DEFAULT: '#4EA8DE',
+          dark: '#1E6F9F',
+        },
+        gray: {
+          700: '#0D0D0D',
+          600: '#1A1A1A',
+          500: '#262626',
+          400: '#333333',
+          300: '#808080',
+          200: '#D9D9D9',
+          100: '#F2F2F2',
+          DEFAULT: '#4EA8DE',
+        },
+        danger: '#E25858',
+      },
     },
   },
   plugins: [],
